1c541a2e67a0d7eca2a9fed30ccb961d294c437b
Marcos Assuncao <assuncao@acm.org>
2022-05-15 16:20:55 -0400

    Examples of class 5

---
+++ cours05-events/callbacks.js
@@ -0,0 +1,31 @@
+console.log(" :: callbacks :: ");
+
+setTimeout(() => console.log("Trois secondes ecoulées..."), 3000);
+
+// ------------
+
+function callback() {
+    console.log("Une fonction de rappel appelée");
+}
+
+setTimeout(callback, 3000);
+setTimeout(() => {console.log("Une autre fonction de rappel appelée");}, 1000);
+setTimeout(() => {console.log("Encore une autre fonction de rappel appelée");}, 1000);
+
+// ------------
+
+let x = 0;
+console.log("Code est exécuté immédiatement | x=" + x);
+
+// Sera exécuté plus tard, dans 1s
+setTimeout(() => {
+    x++;
+    console.log("Code est exécuté après 1 s. | x=" + x);
+
+    setTimeout(() => {
+        x++;
+        console.log("Code est exécuté après 1+1 s. x=" + x);
+    }, 1000)
+}, 1000);
+
+console.log("x=" + x);
