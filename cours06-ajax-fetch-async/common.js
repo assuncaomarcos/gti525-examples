c950b374fdf3b5f2812e86c275d0357497ffaf5e
Marcos Assuncao <assuncao@acm.org>
2023-08-31 13:36:01 -0400

    Refactoring class 6

--- cours06-ajax-fetch-async/common.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    // MaterializeCSS a besoin d'ínitialiser certains eléments tels que les select
-    let elems = document.querySelectorAll('select');
-    let instances = M.FormSelect.init(elems);
-});
--- cours06-ajax-fetch-async/json-examples.js
+++
@@ -1,7 +0,0 @@
-let json = '{"name":"Marie", "age":30, "city":"Montreal"}';
-let obj = JSON.parse(json);
-console.log(obj.name + ", " + obj.age + ", " + obj.city);
-
-let obj2 = {name:"Marie", age:30, city:"Montreal"};
-console.log(JSON.stringify(obj2));
-
